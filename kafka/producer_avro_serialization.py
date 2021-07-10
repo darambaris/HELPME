9c9d3a83f8a150a00f2bd4b055485e9c5e86ce5e
Jessika Darambaris <jessika.darambaris@gmail.com>
2021-07-10 19:53:12 -0300

    creates a consumer that serializes the message to Avro

---
+++ kafka/producer_avro_serialization.py
@@ -0,0 +1,67 @@
+# Please complete the TODO items in the code
+
+import asyncio
+from dataclasses import asdict, dataclass, field
+from io import BytesIO
+import random
+
+from confluent_kafka import Producer
+from faker import Faker
+from fastavro import parse_schema, writer
+
+
+faker = Faker()
+
+BROKER_URL = "PLAINTEXT://localhost:9092"
+
+
+@dataclass
+class ClickEvent:
+    email: str = field(default_factory=faker.email)
+    timestamp: str = field(default_factory=faker.iso8601)
+    uri: str = field(default_factory=faker.uri)
+    number: int = field(default_factory=lambda: random.randint(0, 999))
+
+    json_schema = {
+        "type": "record",
+        "name": "clickEvent",
+        "namespace": "com.udacity.lessons.avro",
+        "fields": [
+            {"name": "email", "type": "string"},
+            {"name": "timestamp", "type": "string"},
+            {"name": "uri", "type": "string"},
+            {"name": "number", "type": "int"},
+        ]
+    }
+    schema = parse_schema(schema=json_schema)
+
+    def serialize(self):
+        """Serializes the ClickEvent for sending to Kafka"""
+        out = BytesIO()
+        writer(out, self.schema, [asdict(self)])
+        return out.getvalue()
+
+
+async def produce(topic_name):
+    """Produces data into the Kafka Topic"""
+    p = Producer({"bootstrap.servers": BROKER_URL})
+    while True:
+        p.produce(topic_name, ClickEvent().serialize())
+        await asyncio.sleep(1.0)
+
+
+def main():
+    """Checks for topic and creates the topic if it does not exist"""
+    try:
+        asyncio.run(produce_consume("consumer.serialization.clicks"))
+    except KeyboardInterrupt as e:
+        print("shutting down")
+
+
+async def produce_consume(topic_name):
+    """Runs the Producer and Consumer tasks"""
+    await asyncio.create_task(produce(topic_name))
+
+
+if __name__ == "__main__":
+    main()
