3cc72f92162648e9896076b9bea276f8856241f4
Jéssika Darambaris <jessika.darambaris@gmail.com>
2021-07-16 16:29:32 -0300

    hot fix

--- MongoDB/install and_run.sh
+++ MongoDB/install and_run.sh
@@ -1,4 +1,4 @@
-!#/bin/bash
+#!/bin/bash
 
 # install MongoDB on MacOs using homebrew
 # Ref: https://docs.mongodb.com/manual/tutorial/install-mongodb-on-os-x/#std-label-brew-installs-dbtools 
@@ -6,7 +6,7 @@ brew tap mongodb/brew
 brew install mongodb-community@5.0
 
 # run MongoDB as a service on MacOs using homebrew
-brew services start mongodb-community # service'll run 127.0.0.1:27017
+brew services start mongodb-community # service'll run at 127.0.0.1:27017
 
 # run MongoDB on MacOS manually as a background process
 mongod --config /usr/local/etc/mongod.conf --fork
